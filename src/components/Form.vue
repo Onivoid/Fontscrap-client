b6726a544bc100b8c56675d82bc9bbcf11daa9e9
Logan Wilquin <Skullyfox.yt@gmail.com>
2023-01-17 15:51:09 +0100

    initial commit

---
+++ src/components/Form.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      <input v-model="inputValue" placeholder="Enter some data" />
+      <button @click="sendData">Send Data</button>
+    </div>
+    <div>
+        <p v-for="font in fonts">
+            {{ font.name }}
+        </p>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios'
+  
+  export default {
+    data() {
+      return {
+        inputValue: '',
+        fonts: []
+      }
+    },
+    methods: {
+        async sendData() {
+            axios.post('http://163.172.154.157:3000/scrap', {
+                url: this.inputValue
+            })
+            .then(response => {
+                this.fonts = response.data;
+                console.log(response.data)
+            })
+            .catch(error => console.error(error));
+        }
+  }
+}
+</script>
+ 
